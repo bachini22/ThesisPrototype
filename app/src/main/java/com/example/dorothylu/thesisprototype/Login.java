59f57ebfc7388eaa3819467820586631c069e821
Adrian Bachini <bachiniadrian@gmail.com>
2016-07-01 17:23:59 +0800

    Added Firebase api

--- app/src/main/java/com/example/dorothylu/thesisprototype/Login.java
+++ app/src/main/java/com/example/dorothylu/thesisprototype/Login.java
@@ -3,11 +3,14 @@ package com.example.dorothylu.thesisprototype;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.client.Firebase;
+
 public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_login);
     }
 }
