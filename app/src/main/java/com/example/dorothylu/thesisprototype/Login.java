8fdf79bdad5fd1ff7db5e90a019b788cd62f4b6e
Adrian Bachini <bachiniadrian@gmail.com>
2016-07-03 17:55:11 +0800

    Updated Firebase API

--- app/src/main/java/com/example/dorothylu/thesisprototype/Login.java
+++ app/src/main/java/com/example/dorothylu/thesisprototype/Login.java
@@ -1,16 +1,21 @@
 package com.example.dorothylu.thesisprototype;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.firebase.client.AuthData;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
@@ -18,14 +23,16 @@ public class Login extends AppCompatActivity {
     private String pass;
     private EditText emailField;
     private EditText passField;
-    Firebase ref;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_login);
-        ref = new Firebase("https://thesis-cb2dc.firebaseio.com/");
+        //ref = new Firebase("https://thesis-cb2dc.firebaseio.com/");
+        mAuth = FirebaseAuth.getInstance();
         Button login = (Button)findViewById(R.id.signin);
         login.setOnClickListener( new View.OnClickListener() {
 
@@ -35,30 +42,74 @@ public class Login extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 emailField = (EditText)findViewById(R.id.emailField);
                 passField = (EditText)findViewById(R.id.passwordField);
-                ref.authWithPassword(emailField.getText().toString(), passField.getText().toString(), authResultHandler);
+                signIn();
             }
         });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+    }
+
+    public void signIn(){
+        System.out.println("entered signIN");
+        mAuth.signInWithEmailAndPassword(emailField.getText().toString(), passField.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // successfully logged in
+
+
+                        if (!task.isSuccessful()) {
+                            System.out.println("signin failed");
+                            Log.w(TAG, "signInWithEmail", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        else{
+                            // sira pa newsfeed
+                            //Intent intent = new Intent(Login.this, NewsFeed.class);
+                            //startActivity(intent);
+                            //finish();
+                        }
+
+
+                    }
+                });
 
     }
 
 
-    //handles authentication
-    Firebase.AuthResultHandler authResultHandler = new Firebase.AuthResultHandler() {
-        @Override
-        public void onAuthenticated(AuthData authData) {
-            // Authenticated successfully with payload authData
-            //go to newsfeed
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
 
 
-        }
-        @Override
-        public void onAuthenticationError(FirebaseError firebaseError) {
-            // Authenticated failed with error firebaseError
-            TextInputLayout email = (TextInputLayout) findViewById(R.id.emailWrapper);
-            email.setErrorEnabled(true);
-            email.setError("Invalid Username and/or Password");
-        }
-    };
+
 
 
 
